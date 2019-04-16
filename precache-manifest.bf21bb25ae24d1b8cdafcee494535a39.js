9169489d926ebf1b0c3074bc2db239f08f57654d
한일석 <x2ever@naver.com>
2019-04-16 23:24:16 +0900

    Updates

---
+++ precache-manifest.bf21bb25ae24d1b8cdafcee494535a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "792f3b8172b35e63ea54",
+    "url": "/madical-register-dapp/static/js/runtime~main.0d06eda0.js"
+  },
+  {
+    "revision": "65c6e2f21f26601b1592",
+    "url": "/madical-register-dapp/static/js/main.c7596080.chunk.js"
+  },
+  {
+    "revision": "f654ce3b140ab8b7ddc9",
+    "url": "/madical-register-dapp/static/js/2.94b12ad5.chunk.js"
+  },
+  {
+    "revision": "65c6e2f21f26601b1592",
+    "url": "/madical-register-dapp/static/css/main.23d17cbe.chunk.css"
+  },
+  {
+    "revision": "f654ce3b140ab8b7ddc9",
+    "url": "/madical-register-dapp/static/css/2.401beb52.chunk.css"
+  },
+  {
+    "revision": "ce05ba4c64fb1641bf34c509914996ea",
+    "url": "/madical-register-dapp/index.html"
+  }
+];
